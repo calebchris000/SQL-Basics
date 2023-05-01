8cf3d114e8f255d950e0c33303ad1461fc709b3d
calebchris000 <calebchris000@gmail.com>
2023-05-01 22:14:25 +0100

    Add queries, data and schema

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
+SELECT * FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT * FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT * FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = TRUE;
+SELECT * FROM animals WHERE NOT (name = 'Gabumon');
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
